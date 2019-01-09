e20f0b0cf776c60e3f7cfcdef749f48c1b408db9
jlauerswi <41702008+jlauerswi@users.noreply.github.com>
2019-01-09 12:30:23 -0500

    Init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+Set-PSReadlineOption -TokenKind Command -BackgroundColor White
+Set-PSReadlineOption -TokenKind Variable -BackgroundColor White
+Set-PSReadlineOption -TokenKind Comment -BackgroundColor White
+Set-PSReadlineOption -TokenKind Keyword -BackgroundColor White
+Set-PSReadlineOption -TokenKind String -BackgroundColor White
+Set-PSReadlineOption -TokenKind Operator -BackgroundColor White
+Set-PSReadlineOption -TokenKind Parameter -BackgroundColor White
+Set-PSReadlineOption -TokenKind Type -BackgroundColor White
+Set-PSReadlineOption -TokenKind Number -BackgroundColor White
+Set-PSReadlineOption -TokenKind Member -BackgroundColor White
+Set-PSReadlineOption -TokenKind Command -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Variable -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Comment -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Keyword -ForegroundColor Black
+Set-PSReadlineOption -TokenKind String -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Operator -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Type -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Number -ForegroundColor Black
+Set-PSReadlineOption -TokenKind Member -ForegroundColor Black
+$Host.PrivateData.ErrorBackgroundColor = 'White'
+$Host.PrivateData.ErrorForegroundColor = 'DarkCyan'
+
+function prompt {
+"" + (get-location) + " " + $env:UserName + "$ "
+}
+
+New-Alias which get-command
